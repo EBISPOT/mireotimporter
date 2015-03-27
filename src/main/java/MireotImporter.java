41349c6913f57d472734873bf1d8e4fa8b31d408
james.malone@gmail.com <james.malone@gmail.com>
2015-03-27 14:52:55 +0000

    First commit mireot importer

---
+++ src/main/java/MireotImporter.java
@@ -0,0 +1,20 @@
+/**
+ * Created by malone on 27/03/2015.
+ */
+
+
+/**
+ * Driver class to run the import code
+ */
+public class MireotImporter {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello welt");
+
+    }
+
+
+
+}
---
+++ src/main/java/MireotManager.java
@@ -0,0 +1,10 @@
+/**
+ * Created by malone on 27/03/2015.
+ */
+public class MireotManager {
+
+
+
+
+
+}
